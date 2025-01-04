cc5949a9e7fdab35976d9705fb8fdc8c9b4e7120
Adventofcode35 <164210703+Adventofcode35@users.noreply.github.com>
2025-01-04 15:40:27 +0530

    Delete Warning.py

--- Warning.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from streamlit_autorefresh import st_autorefresh
-from PIL import Image
-import numpy as np
-import torch
-import torchvision
-from torchvision.models import resnet50, ResNet50_Weights
-import os
-st.title("Image Classifier For Railways")
